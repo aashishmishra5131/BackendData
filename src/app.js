2d5ba112cd70b8b2a08e4c1ace3b4bb365616769
AashishMishra <aashishmishra5131@gmail.com>
2024-02-14 23:14:51 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -12,4 +12,9 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//router import
+import userRouter from './routes/user.router.js';
+
+app.use("/api/v1/users",userRouter)
+
 export{app};
--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
@@ -33,7 +34,7 @@ dotenv.config({
 connectDB()
 
 .then(()=>{
-   app.listen(process.env.PORT||8000,()=>{
+   app.listen(process.env.PORT||5000,()=>{
     console.log(` Server is running at port :${process.env.PORT}`);
    })
    app.on("error",(error)=>{
